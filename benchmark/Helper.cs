6c2bad5a87eee53b59a24af938c3b769e5cd32d6
Robin Choffardet <robin.choffardet@gmail.com>
2022-08-29 23:07:35 +0200

    readme + benchmark

---
+++ benchmark/Helper.cs
@@ -0,0 +1,57 @@
+public static class Helper
+{
+    public static void Deconstruct<T>(this T[] arr, out T first, out T second, out T third)
+    {
+        first = arr[0];
+        second = arr[1];
+        third = arr[2];
+    }
+
+    public static void Deconstruct<T>(this T[] arr, out T first, out T second, out T third, out T fourth)
+    {
+        first = arr[0];
+        second = arr[1];
+        third = arr[2];
+        fourth = arr[3];
+    }
+
+    public static string Humanize(this TimeSpan duration)
+    {
+        if(duration.TotalMicroseconds < 1000) 
+            return $"{Math.Round(duration.TotalMicroseconds, 3)} μs";
+
+        if(duration.TotalNanoseconds < 1000) 
+            return $"{Math.Round(duration.TotalNanoseconds, 3)} ns";
+
+        if(duration.TotalMilliseconds < 1000) 
+            return $"{Math.Round(duration.TotalMilliseconds, 3)} ms";
+
+         return $"{Math.Round(duration.TotalSeconds, 3)} s";
+    }
+
+        public static string Humanize(this float nb, string unit = "")
+    {
+        return Humanize((double) nb, unit);
+    }
+
+    public static string Humanize(this long nb, string unit = "")
+    {
+        return Humanize((double) nb, unit);
+    }
+
+    public static string Humanize(this double nb, string unit = "")
+    {
+        var space = unit == "" ? "" : " ";
+
+        if(nb < 1_000) 
+            return $"{nb}";
+
+        if(nb < 1_000_000) 
+            return $"{Math.Round(nb/1_000, 3)}{space}K{unit}";
+
+        if(nb < 1_000_000_000) 
+            return $"{Math.Round(nb/1_000_000, 3)}{space}M{unit}";
+
+         return $"{Math.Round(nb/1_000_000_000, 3)}{space}G{unit}";
+    }
+}
---
+++ benchmark/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using BetterConsoles.Tables;
+using BetterConsoles.Tables.Configuration;
+using BetterConsoles.Tables.Models;
+
+var dir = Directory.GetCurrentDirectory();
+
+var toBench = Directory.GetFiles(Path.Combine(dir, "to-bench"));
+var datasets = Directory.GetDirectories(Path.Combine(dir, "datasets"));
+
+
+foreach(var dataset in datasets)
+{
+    var peopleNb =  File.ReadLines(Path.Combine(dataset, "people.txt"))
+        .LongCount()
+        .Humanize();
+
+    var friendshipNb =  File.ReadLines(Path.Combine(dataset, "friendship.txt"))
+        .LongCount()
+        .Humanize();
+
+    var searches = File.ReadLines(Path.Combine(dataset, "search.txt"));
+
+    var datasetName = Path.GetFileName(dataset).Substring(2);
+    var datasetNumbers = $"({peopleNb} entries, {friendshipNb} nodes)";
+    Console.WriteLine($"Dataset: {datasetName} {datasetNumbers}");
+
+    var table = new Table("Run", "Duration", "CPU time", "Memory", "A/E");
+    table.Config = TableConfig.Unicode();
+
+    foreach(var exe in toBench)
+    {
+        var runs = new List<Run>();
+        var i = 1;
+
+        foreach(var search in searches)
+        {
+            var (first, second, expected) = search.Split(':');
+
+            var process = new Process();
+            process.StartInfo.WorkingDirectory = dataset;
+            process.StartInfo.FileName = exe;
+            process.StartInfo.Arguments = $"\"{first}\" \"{second}\"";
+            process.StartInfo.RedirectStandardOutput = true;
+            var memory = 0L;
+
+            var sw = new Stopwatch();
+            process.Start();
+
+            while(!process.HasExited)
+            {
+                memory = process.PeakWorkingSet64;
+            }
+
+            process.WaitForExit();
+
+            var duration = sw.Elapsed;
+
+            var output = process.StandardOutput.ReadToEnd().Trim();
+
+            var isExpected = output == expected;
+            runs.Add(
+                new Run(
+                    process.ExitTime - process.StartTime, 
+                    process.TotalProcessorTime,
+                    memory, 
+                    isExpected
+                ));
+
+            i++;
+        }
+
+        table.AddRow(
+            Path.GetFileNameWithoutExtension(exe), 
+            TimeSpan.FromTicks((long) runs.Average(r => r.Duration.Ticks)).Humanize(),
+            TimeSpan.FromTicks((long) runs.Average(r => r.Cpu.Ticks)).Humanize(),
+            runs.Average(r => r.Memory).Humanize("B"), 
+            string.Join(' ', runs.Select(r => r.ExpectedResult ? "✓" : "✗")) 
+        );
+    }
+
+    Console.WriteLine(table.ToString());
+}
+
+
+public record Run(
+    TimeSpan Duration, 
+    TimeSpan Cpu, 
+    float Memory, 
+    bool ExpectedResult
+);
