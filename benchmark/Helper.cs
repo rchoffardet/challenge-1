b994163d2eb009fe137dad1ea6e7e9ad263a68c9
Jean Hominal <jhominal@gmail.com>
2022-08-31 23:07:22 +0200

    Remove unused Deconstruct functions

--- benchmark/Helper.cs
+++ benchmark/Helper.cs
@@ -1,20 +1,5 @@
 public static class Helper
 {
-    public static void Deconstruct<T>(this T[] arr, out T first, out T second, out T third)
-    {
-        first = arr[0];
-        second = arr[1];
-        third = arr[2];
-    }
-
-    public static void Deconstruct<T>(this T[] arr, out T first, out T second, out T third, out T fourth)
-    {
-        first = arr[0];
-        second = arr[1];
-        third = arr[2];
-        fourth = arr[3];
-    }
-
     public static string Humanize(this TimeSpan duration)
     {
         if(duration.TotalMicroseconds < 1000) 
